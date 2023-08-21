89264d3a9fdf4ac89057b3146319dee3d45c8dd0
gabryellesoares <gabryelle.ss3@gmail.com>
2023-08-21 17:39:52 -0300

    add files

--- helper_np.py
+++ helper_np.py
@@ -2,6 +2,7 @@ import os
 import sys
 import math
 import random
+import pickle
 import librosa
 import warnings
 import argparse
@@ -46,83 +47,159 @@ def get_segment(y, sr, segment, split_duration, duration):
     return y[start_sample:end_sample]
 
 
-def process_data(csv_file, audio_path, outfile):
-    df_jamendo = pd.read_csv(csv_file)
-    instruments = np.unique(df_jamendo['instrument'])
-    inst_num = len(instruments)
+def process_demucs(audio_path, outfile):
+    new_dict = {'track_id': [], 'instrument': [], 'predicted': []}
+    inst_openmic = ['bass', 'drums', 'guitar', 'piano', 'voice']
 
-    print('Extracting the vggish features...')
-    X = []
+    for track in os.listdir(audio_path):
+        path = os.path.join(audio_path, track)
 
-    new_dict = {'track_id': [], 'instrument': []}
+        if os.path.isfile(path):
+            print(track)
+            audio, sr = librosa.load(path)
+            audio_splitted = remove_silence(audio)
+            segments = split(audio_splitted, sr, split_duration=5)
+            inst = track.split('_')[0]
+            insts = []
 
-    for _, row in df_jamendo.iterrows():
-        full_path = os.path.join(
-            audio_path, row.path.replace(".mp3", ".low.mp3"))
-        try:
-            y, sr = librosa.load(full_path)
-            new_y = remove_silence(y)
-            segments = split(new_y, sr)
-            indices = list(range(len(segments)))
-            random.shuffle(indices)
-            subsampled_indices = indices[:5]
-
-            for i, idx in enumerate(subsampled_indices):
-                segment = segments[idx]
+            for _, segment in enumerate(segments):
                 _, features = vggish.waveform_to_features(segment, sr)
-                X.append(features)
+                feature_mean = np.mean(features, axis=0, keepdims=True)
+                probs = []
 
-                new_dict['track_id'].append(f'{row.track_id}_{i}')
-                new_dict['instrument'].append(row.instrument)
+                for instrument in inst_openmic:
+                    with open(f'openmic/models/{instrument}.pkl', 'rb') as f:
+                        clf = pickle.load(f)
+                    prob = clf.predict_proba(feature_mean)[0, 1]
+                    probs.append((instrument, prob))
 
-        except:
-            print(traceback.format_exc())
-            print(f"Not found: {full_path}")
-            continue
+                max_prob = max(probs, key=lambda x: x[1])
+                if max_prob[0] == 'voice':
+                    insts.append('vocals')
+                else:
+                    insts.append(max_prob[0])
+
+            most_common_instrument = max(set(insts), key=insts.count)
+            new_dict['track_id'].append(track)
+            new_dict['instrument'].append(inst)
+            new_dict['predicted'].append(most_common_instrument)
+            print('-' * 50)
 
     new_df = pd.DataFrame(new_dict)
-    tracks = np.unique(new_df['track_id'])
-    songs_num = len(tracks)
-    Y_mask = np.zeros([songs_num, inst_num], dtype=bool)
-
-    print('Extracting the labels information...')    
-
-    for _, row in new_df.iterrows():
-        x_pos = int(np.arange(songs_num)[tracks == row.track_id])
-        y_pos = int(np.arange(inst_num)[instruments == row.instrument])
-        Y_mask[x_pos, y_pos] = 1
-
-    print(len(X))
-    X = np.array(X)
-    print(X.shape)
-    print(Y_mask.shape)
-    print(len(tracks))
-    
-    print('Saving NPZ and CSV files...')
-    np.savez(outfile, X=X, Y_mask=Y_mask, track_id=tracks)
-    new_df.to_csv('mtg-jamendo/selected-instruments-splitted.csv', index=False)   
+    new_df.to_csv(outfile, index=False)
     print('Done.')
 
 
+def process_jamendo(audio_path, outfile):
+    df_jamendo = pd.read_csv('mtg-jamendo/selected-instruments.csv')
+    new_dict = {'track_id': [], 'instrument': [], 'predicted': []}
+    inst_openmic = ['bass', 'drums', 'guitar', 'piano', 'voice']
+
+    for idx, row in df_jamendo.iterrows():
+        print(f'track {idx}')
+        full_path = os.path.join(
+            audio_path, row.path.replace(".mp3", ".low.mp3").split('/')[1])
+        try:
+            audio, sr = librosa.load(full_path)
+            audio_splitted = remove_silence(audio)
+            segments = split(audio_splitted, sr, split_duration=5)
+            indices = list(range(len(segments)))
+            random.shuffle(indices)
+            subsampled_indices = indices[:5]
+            inst = row.instrument
+            insts = []
+
+            if inst == 'acousticguitar' or inst == 'electricguitar':
+                inst = 'guitar'
+
+            for i, j in enumerate(subsampled_indices):
+                segment = segments[j]
+                _, features = vggish.waveform_to_features(segment, sr)
+                feature_mean = np.mean(features, axis=0, keepdims=True)
+                probs = []
+
+                for instrument in inst_openmic:
+                    with open(f'openmic/models/{instrument}.pkl', 'rb') as f:
+                        clf = pickle.load(f)
+                    prob = clf.predict_proba(feature_mean)[0, 1]
+                    probs.append((instrument, prob))
+
+                max_prob = max(probs, key=lambda x: x[1])
+                insts.append(max_prob[0])
+        except:
+            print(traceback.format_exc())
+            sys.exit()
+
+        most_common_instrument = max(set(insts), key=insts.count)
+        new_dict['track_id'].append(row.track_id)
+        new_dict['instrument'].append(inst)
+        new_dict['predicted'].append(most_common_instrument)
+        print('-' * 50)
+
+    new_df = pd.DataFrame(new_dict)
+    new_df.to_csv(outfile, index=False)
+    print('Done.')
+
+def process_nsynth(audio_path, outfile):
+    df_nsynth = pd.read_csv('nsynth/nsynth_instruments.csv')
+    new_dict = {'track_id': [], 'instrument': [], 'predicted': []}
+    inst_openmic = ['bass', 'guitar', 'synthesizer', 'voice']
+
+    for idx, row in df_nsynth.iterrows():
+        print(f'track {idx}')
+        full_path = os.path.join(audio_path, row.track_id + '.wav')
+        try:
+            audio, sr = librosa.load(full_path)
+            #audio_splitted = remove_silence(audio)
+            inst = row.instrument
+
+            if inst == 'keyboard':
+                inst = 'synthesizer'
+            elif inst == 'vocal':
+                inst = 'voice'
+            
+            _, features = vggish.waveform_to_features(audio, sr)
+            feature_mean = np.mean(features, axis=0, keepdims=True)
+            probs = []
+            
+            for instrument in inst_openmic:
+                with open(f'openmic/models/{instrument}.pkl', 'rb') as f:
+                    clf = pickle.load(f)
+                prob = clf.predict_proba(feature_mean)[0, 1]
+                probs.append((instrument, prob))
+            
+            max_prob = max(probs, key=lambda x: x[1])
+            new_dict['track_id'].append(row.track_id)
+            new_dict['instrument'].append(inst)
+            new_dict['predicted'].append(max_prob[0])
+            print('-' * 50) 
+        except:
+            print(traceback.format_exc())
+            sys.exit()
+
+    new_df = pd.DataFrame(new_dict)
+    new_df.to_csv(outfile, index=False)
+    print('Done.')
+
 def process_args(args):
-    parser = argparse.ArgumentParser(
-        description='VGGish to NumPy data generator')
-
-    parser.add_argument('--csv_file', default='', type=str,
-                        help='Path to the sparse labels CSV file.')
-    parser.add_argument('--audio_path', default='',
-                        type=str, help='Path to where the audio files are stored.')
-
-    parser.add_argument('--output_file', default='',
-                        type=str, help='Path and name to store the inputs files in a single NPZ file')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--audio_path', default='', type=str)
+    parser.add_argument('--output_file', default='', type=str)
+    parser.add_argument('--type', default='', type=str)
     return parser.parse_args(args)
 
 
 if __name__ == '__main__':
     args = process_args(sys.argv[1:])
 
-    if not args.csv_file or not args.audio_path or not args.output_file:
+    if not args.audio_path or not args.output_file or not args.type:
         raise ValueError(
-            "Both `--csv_file`, `--audio_path` and `--output_file` must be given.")
-
-    process_data(args.csv_file, args.audio_path, args.output_file)
+            "Both `--audio_path`, `--output_file` and `--type` must be given.")
+    print('n' + args.type + 'n')
+    if args.type == 'demucs':
+        print('demucs')
+        process_demucs(args.audio_path, args.output_file)
+    elif args.type == 'jamendo':
+        process_jamendo(args.audio_path, args.output_file)
+    elif args.type == 'nsynth':
+        process_nsynth(args.audio_path, args.output_file)
